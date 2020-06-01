b25372dfa214832df841efb70f70d36a4c9d6789
KoolPie <Philipp@DESKTOP-FU8SL8F>
2020-06-01 12:12:13 +0200

    add accounts controller with register and login action

---
+++ src/Shopping/Shared/Model/Account/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shopping.Shared.Model.Account
+{
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "User name or Email address")]
+        public string LoginName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ src/Shopping/Shared/Model/Account/RegisterResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Shared.Model.Account
+{
+    public class RegisterResult
+    {
+        public bool Successful { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
