8e9e408baf13b9a4ac5542a8ac122a631f0c5fee
KoolPie <Philipp@DESKTOP-FU8SL8F>
2020-06-17 20:43:54 +0200

    added recursive deletion of product category -> product -> shopping list item

--- src/Shopping/Server/Services/Implementations/ProductCategoryRepository.cs
+++ src/Shopping/Server/Services/Implementations/ProductCategoryRepository.cs
@@ -13,7 +13,7 @@ namespace Shopping.Server.Services.Implementations
 {
     public class ProductCategoryRepository : CRUDDbContextBaseImpl<ProductCategory>, IProductCategories
     {
-        public ProductCategoryRepository(ShoppingDbContext context, ILogger<ProductCategory> logger) 
+        public ProductCategoryRepository(ShoppingDbContext context, ILogger<ProductCategory> logger)
             : base(context, logger)
         {
         }
@@ -25,7 +25,7 @@ namespace Shopping.Server.Services.Implementations
             {
                 categories = await _context.Categories.ToListAsync();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 _logger.LogError(e, "Could not load categories");
             }
@@ -35,11 +35,37 @@ namespace Shopping.Server.Services.Implementations
         public override async Task<ProductCategory> GetAsync(string id)
         {
             var category = await _context.Categories.FirstOrDefaultAsync(i => i.Id == id);
-            if(category == null)
+            if (category == null)
             {
                 throw new ItemNotFoundException();
             }
             return category;
+        }
+
+        public override async Task<bool> DeleteByIdAsync(string id)
+        {
+            var products = await GetProductsWithCategory(id);
+            if (products.Count > 0)
+            {
+                foreach (var product in products)
+                {
+                    var shoppinglists = await GetShoppingListsWithProduct(product.Id);
+                    if (shoppinglists.Count > 0)
+                    {
+                        foreach (var list in shoppinglists)
+                        {
+                            var deleteItem = list.Items.FirstOrDefault(i => i.ProductItemId == product.Id);
+                            if (deleteItem != null)
+                            {
+                                list.Items.Remove(deleteItem);
+                            }
+                        }
+                    }
+                    _context.Remove<ProductItem>(product);
+                }
+            }
+
+            return await base.DeleteByIdAsync(id);
         }
 
         public override bool ItemAlreadyExists(ProductCategory item)
@@ -53,6 +79,24 @@ namespace Shopping.Server.Services.Implementations
         {
             existing.Name = update.Name;
             existing.ColorCode = update.ColorCode;
+        }
+
+        private async Task<List<ProductItem>> GetProductsWithCategory(string categoryId)
+        {
+            var products = await _context.Products.ToListAsync();
+            products = products
+                .Where(p => p.CategoryId == categoryId)
+                .ToList();
+
+            return products;
+        }
+        private async Task<List<ShoppingList>> GetShoppingListsWithProduct(string productItemId)
+        {
+            var shoppinglist = await _context.ShoppingLists.ToListAsync();
+            shoppinglist = shoppinglist
+                .Where(i => i.Items.Any(p => p.ProductItemId == productItemId))
+                .ToList();
+            return shoppinglist;
         }
     }
 }
--- src/Shopping/Shared/Services/Interfaces/ICRUDAccess.cs
+++ src/Shopping/Shared/Services/Interfaces/ICRUDAccess.cs
@@ -12,7 +12,6 @@ namespace Shopping.Shared.Services
         Task<T> GetAsync(string id);
         Task<T> CreateAsync(T item);
         Task<T> UpdateAsync(string id, T item);
-        Task<bool> DeleteAsync(T item);
         Task<bool> DeleteByIdAsync(string id);
     }
 }
