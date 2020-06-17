920853c964757c2170bef0bf8c3b9eb29a349414
KoolPie <Philipp@DESKTOP-FU8SL8F>
2020-06-17 18:30:31 +0200

    add js for dropdown

---
+++ src/Shopping/Client/wwwroot/js/custom.js
@@ -0,0 +1,13 @@
+﻿window.onclick = function (event) {
+    var targetClasses = event.target.classList;
+    if (!(targetClasses.contains('context-menu-btn') || targetClasses.contains('context-menu-btn-icon'))) {
+        var dropdowns = this.document.getElementsByClassName("context-menu");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
