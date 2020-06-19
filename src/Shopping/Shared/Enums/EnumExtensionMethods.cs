f11944faf8e49ea29ca85085821ee2fb7de67cc1
KoolPie <Philipp@DESKTOP-FU8SL8F>
2020-06-19 18:40:39 +0200

    add ui fixes in tables and form controls

--- src/Shopping/Shared/Enums/EnumExtensionMethods.cs
+++ src/Shopping/Shared/Enums/EnumExtensionMethods.cs
@@ -20,6 +20,20 @@ namespace Shopping.Shared.Enums
             }
             return GenericEnum.ToString();
         }
+        public static string GetDisplayName(this Enum GenericEnum)
+        {
+            Type genericEnumType = GenericEnum.GetType();
+            MemberInfo[] memberInfo = genericEnumType.GetMember(GenericEnum.ToString());
+            if ((memberInfo != null && memberInfo.Length > 0))
+            {
+                var _Attribs = memberInfo[0].GetCustomAttributes(typeof(System.ComponentModel.DataAnnotations.DisplayAttribute), false);
+                if ((_Attribs != null && _Attribs.Count() > 0))
+                {
+                    return ((System.ComponentModel.DataAnnotations.DisplayAttribute)_Attribs.ElementAt(0)).Name;
+                }
+            }
+            return GenericEnum.ToString();
+        }
 
         public static float GetStepperIncrement(this ProductUnit unit)
         {
