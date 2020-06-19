33be2fa988d52a9e027f2f2b570d540806cc2b0b
KoolPie <Philipp@DESKTOP-FU8SL8F>
2020-06-19 19:52:16 +0200

    add sorting to shopping list item overview table

---
+++ src/Shopping/Client/Enums/ShoppingListOverviewTableColumn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Client.Enums
+{
+    public enum ShoppingListOverviewTableColumn
+    {
+        Done,
+        Name,
+        Amount
+    }
+}
