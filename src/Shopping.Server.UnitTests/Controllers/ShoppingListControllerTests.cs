a6f228669c6ee3e9fb7b994eda5902129042da5d
KoolPie <Philipp@DESKTOP-FU8SL8F>
2020-06-28 21:33:55 +0200

    prepare shopping list unit tests

---
+++ src/Shopping.Server.UnitTests/Controllers/ShoppingListControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using Shopping.Shared.Data;
+using Shopping.Shared.Model.Account;
+using Shopping.Shared.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Shopping.Server.UnitTests.Controllers
+{
+    public class ShoppingListControllerTests
+    {
+
+        [Fact]
+        public async Task GetLists_UserIsAdmin_ReturnsAllLists()
+        {
+            List<ShoppingList> lists = new List<ShoppingList>()
+            {
+            };
+        }
+    }
+}
---
+++ src/Shopping.Server.UnitTests/TestData/UserMocks.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using Shopping.Shared.Model.Account;
+using Shopping.Shared.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Server.UnitTests.TestData
+{
+    public static class UserMocks
+    {
+        public static ShoppingUserModel AdminUser = new ShoppingUserModel()
+        {
+            Id = "123-456",
+            Email = "admin@test.de",
+            UserName = "admin"
+        };
+        public static ShoppingUserModel TestUser = new ShoppingUserModel()
+        {
+            Id = "abc-def",
+            Email = "test@test.de",
+            UserName = "test"
+        };
+
+        public static ICurrentUserProvider GetMockUserProvider(ShoppingUserModel user)
+        {
+            var userRepoMock = new Mock<ICurrentUserProvider>();
+
+            userRepoMock.Setup(m => m.GetUserAsync())
+                .Returns(Task.FromResult(user));
+
+            bool isAdmin = user.UserName.Contains("admin", StringComparison.InvariantCultureIgnoreCase);
+
+            userRepoMock.Setup(m => m.IsUserAdminAsync())
+                .Returns(Task.FromResult(isAdmin));
+
+            return userRepoMock.Object;
+        }
+    }
+}
