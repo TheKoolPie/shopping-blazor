8ca4856ec322a42e4950810c132db8ca2734de36
KoolPie <Philipp@DESKTOP-FU8SL8F>
2020-07-03 13:00:29 +0200

    move services into subfolders, added dbbackup service interface

---
+++ src/Shopping/Shared/Services/Interfaces/IShoppingRepoBackup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.Shared.Services.Interfaces
+{
+    public interface IShoppingRepoBackup
+    {
+        Task ImportDataJsonAsync(string filePath);
+        Task ExportDataJsonAsync(string filePath);
+    }
+}
