bf628b84b7cdd21dbec423d428a697e9aeaeac35
KoolPie <philipp.noecker@gmx.de>
2020-11-25 20:29:51 +0100

    add migrations for remove of color code

--- src/Shopping/Server/Migrations/Shopping/20201125192204_Remove-Category-Color.Designer.cs
+++ src/Shopping/Server/Migrations/Shopping/20201125192204_Remove-Category-Color.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shopping.Server.Data;
 
 namespace Shopping.Server.Migrations.Shopping
 {
     [DbContext(typeof(ShoppingDbContext))]
-    partial class ShoppingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201125192204_Remove-Category-Color")]
+    partial class RemoveCategoryColor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
