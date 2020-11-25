16f1ace4138ba82155043faff42c6b999fad3554
KoolPie <philipp.noecker@gmx.de>
2020-11-25 20:11:18 +0100

    add init migrations

---
+++ src/Shopping/Server/Migrations/Shopping/20201125190855_Init.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shopping.Server.Migrations.Shopping
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    ColorCode = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShoppingLists",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    ListDate = table.Column<DateTime>(nullable: false),
+                    OwnerId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoreChains",
+                columns: table => new
+                {
+                    StoreChainId = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    PriceCategory = table.Column<int>(nullable: false),
+                    Url = table.Column<string>(nullable: true),
+                    Logo = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    CreatorId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoreChains", x => x.StoreChainId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGroupMembers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    UserGroupId = table.Column<string>(nullable: false),
+                    MemberId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroupMembers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGroups",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    OwnerId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    Unit = table.Column<int>(nullable: false),
+                    CategoryId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    StoreId = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: false),
+                    HouseNumber = table.Column<string>(nullable: false),
+                    PostalCode = table.Column<int>(nullable: false),
+                    City = table.Column<string>(nullable: false),
+                    PriceCategory = table.Column<int>(nullable: false),
+                    StoreChainId = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    CreatorId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.StoreId);
+                    table.ForeignKey(
+                        name: "FK_Stores_StoreChains_StoreChainId",
+                        column: x => x.StoreChainId,
+                        principalTable: "StoreChains",
+                        principalColumn: "StoreChainId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGroupShoppingLists",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    UserGroupId = table.Column<string>(nullable: true),
+                    ShoppingListId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroupShoppingLists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserGroupShoppingLists_ShoppingLists_ShoppingListId",
+                        column: x => x.ShoppingListId,
+                        principalTable: "ShoppingLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserGroupShoppingLists_UserGroups_UserGroupId",
+                        column: x => x.UserGroupId,
+                        principalTable: "UserGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShoppingListItems",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    ProductItemId = table.Column<string>(nullable: true),
+                    ShoppingListId = table.Column<string>(nullable: true),
+                    Amount = table.Column<float>(nullable: false),
+                    Done = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingListItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShoppingListItems_Products_ProductItemId",
+                        column: x => x.ProductItemId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ShoppingListItems_ShoppingLists_ShoppingListId",
+                        column: x => x.ShoppingListId,
+                        principalTable: "ShoppingLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StoreProductCategories",
+                columns: table => new
+                {
+                    StoreProductCategoryId = table.Column<string>(nullable: false),
+                    StoreId = table.Column<string>(nullable: true),
+                    ProductCategoryId = table.Column<string>(nullable: true),
+                    RankingValue = table.Column<int>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoreProductCategories", x => x.StoreProductCategoryId);
+                    table.ForeignKey(
+                        name: "FK_StoreProductCategories_Categories_ProductCategoryId",
+                        column: x => x.ProductCategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StoreProductCategories_Stores_StoreId",
+                        column: x => x.StoreId,
+                        principalTable: "Stores",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingListItems_ProductItemId",
+                table: "ShoppingListItems",
+                column: "ProductItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingListItems_ShoppingListId",
+                table: "ShoppingListItems",
+                column: "ShoppingListId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoreProductCategories_ProductCategoryId",
+                table: "StoreProductCategories",
+                column: "ProductCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoreProductCategories_StoreId",
+                table: "StoreProductCategories",
+                column: "StoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stores_StoreChainId",
+                table: "Stores",
+                column: "StoreChainId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroupShoppingLists_ShoppingListId",
+                table: "UserGroupShoppingLists",
+                column: "ShoppingListId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroupShoppingLists_UserGroupId",
+                table: "UserGroupShoppingLists",
+                column: "UserGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingListItems");
+
+            migrationBuilder.DropTable(
+                name: "StoreProductCategories");
+
+            migrationBuilder.DropTable(
+                name: "UserGroupMembers");
+
+            migrationBuilder.DropTable(
+                name: "UserGroupShoppingLists");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Stores");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingLists");
+
+            migrationBuilder.DropTable(
+                name: "UserGroups");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "StoreChains");
+        }
+    }
+}
